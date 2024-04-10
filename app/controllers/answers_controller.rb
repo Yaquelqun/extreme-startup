d51a2774b0aa099c7a1c4f77509a4c7de2aa707f
Yaquelqun <sandjiv.parassou@gmail.com>
2024-04-10 19:35:42 +0200

    second question

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,19 +1,26 @@
 class AnswersController < ApplicationController
   def show
     question = params['q']
-    id, formatted_question = question.split(':').map(&:strip)
-    answer = answer_question(formatted_question)
+    id, formatted_question, params = question.split(':').map(&:strip)
+    answer = answer_question(formatted_question, params)
     render json: answer
   end
 
   private
 
-  def answer_question(question)
-     check_addition = question.match /what is (\d+) plus (\d+)/
-     return answer_addition(check_addition) if check_addition
+  def answer_question(question, arguments = nil)
+    check_addition = question.match /what is (\d+) plus (\d+)/
+    return answer_addition(check_addition) if check_addition
+    check_largest = question.match /which of the following numbers is the largest/
+    return answer_largest(arguments) if check_largest
   end
 
   def answer_addition(match)
     match[1].to_i + match[2].to_i
+  end
+
+  def answer_largest(string_arguments)
+    array = string_arguments.split(', ').map(&:to_i)
+    array.max
   end
 end
--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -14,11 +14,11 @@ RSpec.describe AnswersController, type: :request do
 
   context 'on addition' do
     before do
-      get '/', params: {"q" => "613e2e30: what is 4 plus 3"}
+      get '/', params: {"q" => "5d861f20: which of the following numbers is the largest: 73, 178"}
     end
 
     it "succeeds" do
-      expect(response.body).to eql "7"
+      expect(response.body).to eql "178"
     end
   end
 end
